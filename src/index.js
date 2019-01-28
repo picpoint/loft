35319d206d49a117ba1cf88af9ed5eb094525889
rmtar <picpoint@yandex.ru>
2019-01-28 23:03:23 +0300

    first task

---
+++ src/index.js
@@ -0,0 +1,39 @@
+/* ДЗ 6.1 - Асинхронность и работа с сетью */
+
+/**
+ * Функция должна создавать Promise, который должен быть resolved через seconds секунду после создания
+ *
+ * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
+ * @return {Promise}
+ */
+// задача 1 ++++++++++++++++++++++++++++++
+function delayPromise(seconds) {
+	return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve();
+		}, 1000);
+	});
+
+}
+
+
+
+/**
+ * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
+ * https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ * Элементы полученного массива должны быть отсортированы по имени города
+ *
+ * @return {Promise<Array<{name: String}>>}
+ */
+function loadAndSortTowns() {
+    return new Promise(function (resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+      req.send();
+		});
+}
+
+export {
+    delayPromise,
+    loadAndSortTowns
+};
